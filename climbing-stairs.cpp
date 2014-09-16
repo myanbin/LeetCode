f925f114c99851424269fc36d8de26f83a57a1a4
Yanbin Ma <yianbin@gmail.com>
2014-09-16 21:16:06 +0800

    Update code

--- climbing-stairs.cpp
+++ climbing-stairs.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int climbStairs (int n) {
+	if (n < 2) return n;
+	int fn = 0,
+	  fn_1 = 1,
+	  fn_2 = 0;
+	for (int i = 2; i <= n; i++) {
+		fn = fn_1 + fn_2;
+		fn_2 = fn_1;
+		fn_1 = fn;
+	}
+	return fn;
 }
 
 int main () {
